06f9063a77e033527083aa683a2cc3b8746e4a00
malunlun <hebeimalunlun@163.com>
2025-07-26 20:08:42 +0800

    Add TestMethod

--- CSharpFeatures/TestCSharpFeatures/UnitTest1.cs
+++ CSharpFeatures/TestCSharpFeatures/UnitTest1.cs
@@ -8,10 +8,12 @@ namespace TestCSharpFeatures
         {
             Assert.AreEqual(1, 1);
         }
+        [TestMethod]
         public void TestMethod2()
         {
             Assert.AreEqual(1, 2);
         }
+        [TestMethod]
         public void TestMethod3()
         {
             Assert.AreEqual(2, 2);
